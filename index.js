48c41d0728612824ccafef57d1d8703f5ddac185
Allie LeFever <alefever39@gmail.com>
2022-04-05 14:30:26 -0600

    commit tree 0e3b7516b8dc1e74cffa9dbce3f7dc18c767d422

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function increment(num1) {
+    num1++;
+    return num1;
+}
+
+function decrement(num1) {
+    num1--;
+    return num1;
+}
+
+function makeInt(input) {
+    return parseInt(input, 10);
+}
+
+function preserveDecimal(input) {
+    return parseFloat(input);
+}
